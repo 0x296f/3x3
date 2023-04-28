367c28cdf7cf6a1de38db97874dfd3d2ccf8dcbf
0x296 <128113401+0x296b@users.noreply.github.com>
2023-04-28 16:18:56 +0300

    Add files via upload

---
+++ widget.h
@@ -0,0 +1,63 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QGraphicsView>
+#include <QFileDialog>
+#include <QLabel>
+
+
+class ImageView : public QGraphicsView {
+public:
+    ImageView(QWidget* parent = nullptr) : QGraphicsView(parent) {}
+
+protected:
+    void resizeEvent(QResizeEvent* event) override {
+        QGraphicsView::resizeEvent(event);
+        fitInView(sceneRect(), Qt::KeepAspectRatio);
+    }
+};
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public slots:
+
+    void _transform();
+    void LoadButtonClicked();
+    void ShowImage();
+    void SaveImage();
+    void Search();
+    void onOkButtonClicked();
+
+public:
+
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+    QSize size;
+
+    int _width = 800;
+    int _height = 500;
+
+    QLabel* width_height;
+    QLabel* label;
+
+    QLabel* info;
+
+    QString _Width ;
+    QString file_name;
+    QString _targetBrightness;
+
+    QPixmap QPixmap_filteredImage;
+    QPixmap SavePixmap;
+    QPixmap imgs;
+    QImage _image;
+    QImage m_filteredImage;
+
+    QGraphicsScene *scene_for_view = nullptr;
+    QGraphicsView * view = nullptr;
+};
+
+#endif // WIDGET_H
